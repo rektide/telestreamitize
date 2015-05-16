b9b9b8c44701f3afe48c3d63194dbac9217c53a2
rektide <rektide@voodoowarez.com>
2015-05-16 18:54:20 -0400

    Static require() statements. Fix channel-pump main's use of promise.

--- channel-pump.js
+++ channel-pump.js
@@ -1,21 +1,38 @@
-function pump(){
-	var names= require('./list-names')()
-	var filteredNames= names.filter(require('./filter-name')('org\\.freedesktop\\.Telepathy\\.Connection\\.\\w+\\.irc\\..*'))
-	var channels= filteredNames.map(require('./list-channels')()).then(require('./flatten'))
-	//var filtered2= filtered1.then(require('./filter-channel')())
-	var emitter= new (require('events').EventEmitter)()
+var events= require('events'),
+  listNames= require('./list-names'),
+  filterNames= require('./filter-name'),
+  flatten= require('./flatten'),
+  listChannels= require('./list-channels'),
+  openMessages= require('./open-messages')
+
+function pump(opts){
+	var nameFilter= opts && opts.nameFilter|| 'org\\.freedesktop\\.Telepathy\\.Connection\\.\\w+\\.irc\\..*'
+	var channelFilter= opts && opts.channelFilter|| '#node.dc'
+
+	var names= listNames()
+	var filteredNames= names.filter(filterNames(nameFilter))
+	var channels= filteredNames.map(listChannels()).then(flatten)
+	var filteredChannels= channels.filter(function(channel){
+		return channel.room.indexOf(channelFilter) === -1
+	})
+	var emitter= new (events.EventEmitter)()
 	emitter.names= filteredNames
 	emitter.channels= channels
-	var messages= channels.map(require('./open-messages')({emitter: emitter}))
+	var messages= channels.map(openMessages({emitter: emitter}))
 	return messages.thenReturn(emitter)
 }
 module.exports= pump
 
 if(module === require.main){
-	var emitter= pump()
-	emitter.then(function(){
+	var _emitter= pump()
+	_emitter.then(function(emitter){
+		emitter.channels.then(function(channels){
+			console.log('channels', channels)
+		})
 		emitter.on('#node.dc', function(){
 			console.log('yeah!', JSON.stringify(arguments))
 		})
+	}, function(err){
+		console.error('error connecting', err)
 	})
 }
